3ececf65d7068443157491e396e9c5173e19716b
torian austin <torian03@outlook.com>
2023-04-23 16:37:22 -0700

    forgot to update the imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,8 @@ import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
-import theme from "../utils/theme";
-import createEmotionCache from "../utils/createEmotionCache";
+import theme from "../../lib/utils/theme";
+import createEmotionCache from "../../lib/utils/createEmotionCache";
 import { SessionProvider } from "next-auth/react"
 
 // Client-side cache, shared for the whole session of the user in the browser.
