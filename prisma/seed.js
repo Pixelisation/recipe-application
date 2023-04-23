70019cff87eb9c2249e939f780797b0595c32786
torian austin <torian03@outlook.com>
2023-04-22 21:15:26 -0700

    seed

---
+++ prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: {
+      email: "test@test.com",
+    },
+    update: {},
+    create: {
+      email: "test@test.com",
+      name: "test user",
+      password: 
+      "f4fe4fg4tb4ugbh487g4g7f4ty948gfbryubgduigs47gt8934t2er4y7fvgeruyfgvberfgv84",
+    },
+  });
+  console.log({ user });
+}
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
